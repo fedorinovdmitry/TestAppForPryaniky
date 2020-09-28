a15212a5b68be36d1b4be7849917ad40f9e169d4
Dmitry Fedorinov <skinklass@mail.ru>
2020-09-28 09:52:02 +0300

    Initial Commit

---
+++ TestAppForPryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppForPryaniky
+//
+//  Created by Дмитрий Федоринов on 28.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
